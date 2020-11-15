4872a2795b53d704948c5b20acdb93533a2296e7
ARUN K <74363128+ARUNMEAN@users.noreply.github.com>
2020-11-15 12:49:15 +0530

    Add files via upload

---
+++ anothersum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    float b,s;
+    printf("\nenter integer and decimal number");
+    scanf("%d%f",&a,&b);
+    s=a+b;
+    printf("\nssum is %f",s);
+}
