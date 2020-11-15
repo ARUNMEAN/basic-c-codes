68d6ad7cbf1bc4add5d9ee3c0398b394db282ae1
ARUN K <74363128+ARUNMEAN@users.noreply.github.com>
2020-11-15 12:07:17 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    
+    float a,b,c,av;
+    printf("enter 3 numbers");
+    scanf("%f%f%f",&a,&b,&c);
+    av=(a+b+c)/3;
+    printf("average is %f",av);
+    
+}
+    
+    
