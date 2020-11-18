f6be699dc98f019ea6418ac687ae72a7199cc036
ARUN K <74363128+ARUNMEAN@users.noreply.github.com>
2020-11-18 11:39:32 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[10];
+    printf("\nenter the character");
+    gets(a);
+    printf("\ncharacter you entered is\n");
+    puts(a);
+}
