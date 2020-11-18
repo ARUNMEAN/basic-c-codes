cf913e8c85763141a016c7d6c699a92139fe1db0
ARUN K <74363128+ARUNMEAN@users.noreply.github.com>
2020-11-18 15:38:10 +0530

    Add files via upload

---
+++ evenprint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("\nenter the number");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        if(i%2==0)
+            printf("\n%d",i);
+    
+        
+    }
+    return 0;
+}
